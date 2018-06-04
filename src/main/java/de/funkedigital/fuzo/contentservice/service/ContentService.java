bae3c4b23200cc7af536eef9753c423cbac0d96f
kazi-funkedigital <k.aziz@funkedigital.de>
2018-06-04 15:56:12 +0200

    Initial Commit: refactored switch statement to functional programming.

--- src/main/java/de/funkedigital/fuzo/contentservice/service/ContentService.java
+++ src/main/java/de/funkedigital/fuzo/contentservice/service/ContentService.java
@@ -11,7 +11,15 @@ import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.BiFunction;
+import java.util.function.Function;
 
+import javax.annotation.PostConstruct;
+
+import javafx.beans.property.ObjectProperty;
 import reactor.core.publisher.Mono;
 
 @Service
@@ -22,15 +30,27 @@ public class ContentService {
     private static final String PAYLOAD_FIELD = "payload";
     private final ContentRepo contentRepo;
     private final ObjectMapper objectMapper;
+    private final TransformContentService transformContentService;
+    private Map<ActionType, BiFunction<JsonNode, JsonNode, Mono<Content>>> transformerActionMap = new HashMap<>();
 
     public enum ActionType {
-        CREATE, UPDATE
+        CREATE, UPDATE, DELETE
     }
 
     ContentService(ContentRepo contentRepo,
-                   ObjectMapper objectMapper) {
+                   ObjectMapper objectMapper,
+                   TransformContentService transformContentService
+    ) {
         this.contentRepo = contentRepo;
         this.objectMapper = objectMapper;
+        this.transformContentService = transformContentService;
+    }
+
+    @PostConstruct
+    private void constructTransformerAction() {
+        transformerActionMap.put(ActionType.CREATE, transformContentService);
+        transformerActionMap.put(ActionType.UPDATE, transformContentService);
+
     }
 
     public Mono<String> get(Long id) {
@@ -44,16 +64,14 @@ public class ContentService {
             JsonNode actionTypeNode = node.get(ACTION_TYPE_FIELD);
             if (actionTypeNode != null && !StringUtils.isEmpty(actionTypeNode.asText())) {
                 ActionType actionType = ActionType.valueOf(actionTypeNode.asText().toUpperCase());
-                switch (actionType) {
-                    case UPDATE:
-                    case CREATE:
-                        return contentRepo.save(new Content(articleIdNode.asLong(),
-                                objectMapper.writeValueAsString(node.get(PAYLOAD_FIELD))));
-                    default:
-                        throw new UnsupportedOperationException(actionType.toString());
-                }
+
+                return Optional.ofNullable(transformerActionMap.get(actionType)).map(m -> {
+                    return m.apply(node, articleIdNode);
+                }).orElseThrow(() -> new UnsupportedOperationException(actionType.toString()));
             }
         }
         throw new RequiredFieldsException();
     }
+
+
 }
---
+++ src/main/java/de/funkedigital/fuzo/contentservice/service/TransformContentService.java
@@ -0,0 +1,52 @@
+package de.funkedigital.fuzo.contentservice.service;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import de.funkedigital.fuzo.contentservice.models.Content;
+import de.funkedigital.fuzo.contentservice.repo.ContentRepo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
+import reactor.core.publisher.Mono;
+
+/**
+ * Created By {kazi}
+ */
+@Component
+public class TransformContentService  implements BiFunction<JsonNode,JsonNode,Mono<Content>>{
+    private static final Logger LOGGER = LoggerFactory.getLogger(TransformContentService.class);
+
+
+    private static final String PAYLOAD_FIELD = "payload";
+
+    private final ContentRepo contentRepo;
+    private final ObjectMapper objectMapper;
+
+    public TransformContentService(ContentRepo contentRepo,ObjectMapper objectMapper) {
+        this.contentRepo = contentRepo;
+        this.objectMapper = objectMapper;
+    }
+
+    @Override
+    public Mono<Content> apply(JsonNode node, JsonNode articleIdNode)  {
+        Mono<Content> savedContent =null;
+
+        try {
+            savedContent=contentRepo.save(new Content(articleIdNode.asLong(),
+                    objectMapper.writeValueAsString(node.get(PAYLOAD_FIELD))));
+        }
+        catch (IOException ex)
+        {
+            LOGGER.error("ERROR while transforming : ",ex);
+        }
+        return  savedContent;
+
+    }
+}
