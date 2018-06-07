9751fc168a42102c944ccbd630167e1df7a90d04
Ricardo Baumann <ricardo.luis.baumann@gmail.com>
2018-06-07 14:26:53 +0200

    removing unused code

--- src/main/java/de/funkedigital/fuzo/contentservice/exceptions/RequiredFieldsException.java
+++
@@ -1,8 +0,0 @@
-package de.funkedigital.fuzo.contentservice.exceptions;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
-public class RequiredFieldsException extends RuntimeException {
-}
