5d8264aba130b950c794b9e1c543878a77d326f3
Ricardo Baumann <ricardo.luis.baumann@gmail.com>
2018-06-14 16:50:04 +0200

    improving script

--- deploy.sh
+++ deploy.sh
@@ -7,3 +7,4 @@ docker build -t fuzo-content-service:${number}  .
 docker tag fuzo-content-service:${number} 656201843059.dkr.ecr.eu-central-1.amazonaws.com/fuzo:latest
 aws ecr create-repository --repository-name fuzo
 docker push 656201843059.dkr.ecr.eu-central-1.amazonaws.com/fuzo
+rm credentials
