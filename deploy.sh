202b8509249745cf690da9e7abda0a1faa3b3f56
Ricardo Luis Baumann <gemini.richard@gmail.com>
2018-06-15 10:12:58 +0200

    docker_push_support (#12)
    * docker buiold and push command
    
    * docker build and push command
    
    * improving script

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+eval `aws ecr get-login --no-include-email`
+cp ~/.aws/credentials .
+number=`git rev-parse --short HEAD`
+echo ${number}
+docker build -t fuzo-content-service:${number}  .
+docker tag fuzo-content-service:${number} 656201843059.dkr.ecr.eu-central-1.amazonaws.com/fuzo:latest
+aws ecr create-repository --repository-name fuzo
+docker push 656201843059.dkr.ecr.eu-central-1.amazonaws.com/fuzo
+rm credentials
