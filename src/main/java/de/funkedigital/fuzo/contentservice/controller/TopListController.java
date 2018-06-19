8917253bd37bd34972ba1fef29867f26b1df9f60
Ricardo Baumann <ricardo.luis.baumann@gmail.com>
2018-06-19 13:53:39 +0200

    adding cache support for toplists

--- src/main/java/de/funkedigital/fuzo/contentservice/controller/TopListController.java
+++ src/main/java/de/funkedigital/fuzo/contentservice/controller/TopListController.java
@@ -3,6 +3,7 @@ package de.funkedigital.fuzo.contentservice.controller;
 import de.funkedigital.fuzo.contentservice.service.TopListService;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,6 +19,7 @@ public class TopListController {
         this.topListService = topListService;
     }
 
+    @Cacheable("toplists")
     @GetMapping("/toplists/")
     public Flux<String> topList() {
         return topListService.getTopListContents();
